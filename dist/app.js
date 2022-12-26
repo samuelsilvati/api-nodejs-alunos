4f387380a9726150b827c8e713914dada291f8c0
samuel <samuelbmx123@gmail.com>
2022-12-26 00:42:57 -0300

    rebuild app

--- dist/app.js
+++ dist/app.js
@@ -21,10 +21,7 @@ class App {
   middlewares() {
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
-    this.app.use(
-      '/images/',
-      _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')),
-    );
+    this.app.use('/img/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'img')));
   }
 
   routes() {
