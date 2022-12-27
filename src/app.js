3d5691858c48b22dff5a0069ed410e7c57c487b8
samuel <samuelbmx123@gmail.com>
2022-12-27 16:50:07 -0300

    configure cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 import dotenv from 'dotenv';
 import { resolve } from 'path';
-
-dotenv.config();
-
 import './database';
+
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import imageRoutes from './routes/imageRoutes';
+
+dotenv.config();
+
+const whiteList = ['http://localhost:3001'];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -21,10 +34,15 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use('/img/', express.static(resolve(__dirname, '..', 'uploads', 'img')));
+    this.app.use(
+      '/img/',
+      express.static(resolve(__dirname, '..', 'uploads', 'img')),
+    );
   }
 
   routes() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
     this.app.use('/tokens/', tokenRoutes);
