b666cdf291b267b264507e1b7fa2c403f9f41bec
samuel <samuelbmx123@gmail.com>
2022-12-28 17:01:02 -0300

    disable cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import './database';
 
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
@@ -13,16 +13,16 @@ import imageRoutes from './routes/imageRoutes';
 
 dotenv.config();
 
-const whiteList = ['http://localhost:3000', 'https://www.google.com.br'];
-const corsOptions = {
-  origin(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+// const whiteList = ['http://localhost:3000', 'https://www.google.com.br'];
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whiteList.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 
 class App {
   constructor() {
@@ -41,7 +41,7 @@ class App {
   }
 
   routes() {
-    this.app.use(cors(corsOptions));
+    // this.app.use(cors(corsOptions));
     this.app.use(helmet());
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
