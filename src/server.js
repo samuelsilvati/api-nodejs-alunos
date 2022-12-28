2520db5cf169569862cf1f89344a5bf8da87a25d
samuel <samuelbmx123@gmail.com>
2022-12-28 16:50:58 -0300

    add access controll allow headers

--- src/server.js
+++ src/server.js
@@ -1,4 +1,18 @@
 import app from './app';
 
+// Add Access-Control-Allow-Headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
+app.get('/api', (req, res) => {
+  res.json('Hello');
+});
+
 const port = process.env.APP_PORT;
 app.listen(port);
