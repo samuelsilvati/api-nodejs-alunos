d0aedde75a393b3a3029aef75947be8fadbbd007
samuel <samuelbmx123@gmail.com>
2022-12-28 22:55:57 -0300

    build app

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,28 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
-
-_dotenv2.default.config();
-
 require('./database');
+
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _imageRoutes = require('./routes/imageRoutes'); var _imageRoutes2 = _interopRequireDefault(_imageRoutes);
+
+_dotenv2.default.config();
+
+const whiteList = ['http://localhost:3000', 'https://www.google.com.br'];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -21,15 +34,29 @@ class App {
   middlewares() {
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
-    this.app.use('/img/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'img')));
+    this.app.use(
+      '/img/',
+      _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'img')),
+    );
   }
 
   routes() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use('/', _homeRoutes2.default);
     this.app.use('/users/', _userRoutes2.default);
     this.app.use('/tokens/', _tokenRoutes2.default);
     this.app.use('/alunos/', _alunoRoutes2.default);
     this.app.use('/images/', _imageRoutes2.default);
+    // Add Access-Control-Allow-Headers
+    this.app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+      next();
+    });
   }
 }
 
